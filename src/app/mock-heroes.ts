755877bf37a36da2a5d2456ae91c56527275a117
Huy <anhuy121@gmail.com>
2019-10-05 10:47:21 +0700

    initial commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr Nice' },
+  { id: 12, name: 'Narcs' },
+  { id: 13, name: 'Bombast' },
+  { id: 14, name: 'Celestas' },
+  { id: 15, name: 'Magneto' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dyanna' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
---
+++ src/app/region/region.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-region',
+  templateUrl: './region.component.html',
+  styleUrls: ['./region.component.css']
+})
+export class RegionComponent implements OnInit {
+
+  constructor() { }
+
+  hero = 'village';
+
+  ngOnInit() {
+  }
+
+}
